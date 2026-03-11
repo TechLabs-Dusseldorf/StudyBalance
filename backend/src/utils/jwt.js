d55ad280e588fa6a9fa145a3c3c35346ae752ea2
AchilovMirzojafar <jafarachilov3@gmail.com>
2026-03-11 13:46:54 +0500

    Request changes done

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -3,4 +3,11 @@ const jwt = require('jsonwebtoken');
 const signJwt = ({ payload, secret, expiresIn }) =>
   jwt.sign(payload, secret, { expiresIn });
 
-module.exports = { signJwt };
+const verifyJwt = ({ token, secret }) => {
+  try {
+    return jwt.verify(token, secret);
+  } catch (err) {
+    return null;
+  }
+};
+module.exports = { signJwt, verifyJwt };
